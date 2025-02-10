60f0618c372e36a60ca23c5ebe9ae22bc794823d
AnPhong1808 <137673484+AnPhong1808@users.noreply.github.com>
2025-02-10 15:41:57 +0700

    Update api.js

--- src/pages/api.js
+++ src/pages/api.js
@@ -1,69 +1,70 @@
 export const preloadData = async (apiKey, userId) => {
   try {
-    // Thực hiện từng yêu cầu fetch riêng lẻ với .catch() để tránh toàn bộ Promise bị lỗi
-    const signalData = await fetch("http://admin.tducoin.com/api/signal", {
-      method: "GET",
-      headers: {
-        "x-api-key": apiKey,
-        "Content-Type": "application/json",
-      },
-    }).then((res) => res.json()).catch(err => { console.error("Error fetching signalData:", err); return {}; });
+    const [signalData, channelData, quizData, newsData, userData, courseData, charityData, giftData] = await Promise.all([
+      fetch("https://admin.tducoin.com/api/signal", { // Đổi từ http sang https
+        method: "GET",
+        headers: {
+          "x-api-key": apiKey,
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json()),
 
-    const channelData = await fetch("http://admin.tducoin.com/api/signal/channel", {
-      method: "GET",
-      headers: {
-        "x-api-key": apiKey,
-        "Content-Type": "application/json",
-      },
-    }).then((res) => res.json()).catch(err => { console.error("Error fetching channelData:", err); return {}; });
+      fetch("https://admin.tducoin.com/api/signal/channel", { // Đổi từ http sang https
+        method: "GET",
+        headers: {
+          "x-api-key": apiKey,
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json()),
 
-    const quizData = await fetch("http://admin.tducoin.com/api/quiz", {
-      method: "GET",
-      headers: {
-        "x-api-key": apiKey,
-        "Content-Type": "application/json",
-      },
-    }).then((res) => res.json()).catch(err => { console.error("Error fetching quizData:", err); return {}; });
+      fetch("https://admin.tducoin.com/api/quiz", { // Đổi từ http sang https
+        method: "GET",
+        headers: {
+          "x-api-key": apiKey,
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json()),
 
-    const newsData = await fetch("http://admin.tducoin.com/api/news", {
-      method: "GET",
-      headers: {
-        "x-api-key": apiKey,
-        "Content-Type": "application/json",
-      },
-    }).then((res) => res.json()).catch(err => { console.error("Error fetching newsData:", err); return {}; });
+      fetch("https://admin.tducoin.com/api/news", { // Đổi từ http sang https
+        method: "GET",
+        headers: {
+          "x-api-key": apiKey,
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json()),
 
-    const userData = await fetch(`http://admin.tducoin.com/api/webappuser/${userId}`, {
-      method: "GET",
-      headers: {
-        "x-api-key": apiKey,
-        "Content-Type": "application/json",
-      },
-    }).then((res) => res.json()).catch(err => { console.error("Error fetching userData:", err); return {}; });
+      fetch(`https://admin.tducoin.com/api/webappuser/${userId}`, { // Đổi từ http sang https
+        method: "GET",
+        headers: {
+          "x-api-key": apiKey,
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json()),
 
-    const courseData = await fetch(`http://admin.tducoin.com/api/course/${userId}`, {
-      method: "GET",
-      headers: {
-        "x-api-key": apiKey,
-        "Content-Type": "application/json",
-      },
-    }).then((res) => res.json()).catch(err => { console.error("Error fetching courseData:", err); return {}; });
+      fetch(`https://admin.tducoin.com/api/course/${userId}`, { // Đổi từ http sang https
+        method: "GET",
+        headers: {
+          "x-api-key": apiKey,
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json()),
 
-    const charityData = await fetch("http://admin.tducoin.com/api/charity", {
-      method: "GET",
-      headers: {
-        "x-api-key": apiKey,
-        "Content-Type": "application/json",
-      },
-    }).then((res) => res.json()).catch(err => { console.error("Error fetching charityData:", err); return {}; });
+      fetch("https://admin.tducoin.com/api/charity", { // Đổi từ http sang https
+        method: "GET",
+        headers: {
+          "x-api-key": apiKey,
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json()),
 
-    const giftData = await fetch("http://admin.tducoin.com/api/gift", {
-      method: "GET",
-      headers: {
-        "x-api-key": apiKey,
-        "Content-Type": "application/json",
-      },
-    }).then((res) => res.json()).catch(err => { console.error("Error fetching giftData:", err); return {}; });
+      fetch("https://admin.tducoin.com/api/gift", { // Đổi từ http sang https
+        method: "GET",
+        headers: {
+          "x-api-key": apiKey,
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json()),
+    ]);
 
     return {
       signalData: signalData?.data || [],
