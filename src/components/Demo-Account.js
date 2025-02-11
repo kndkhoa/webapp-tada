5cfc3a1799822abbf76afb22cb6527b861b685ce
AnPhong1808 <137673484+AnPhong1808@users.noreply.github.com>
2025-02-11 19:05:00 +0700

    Update Demo-Account.js

--- src/components/Demo-Account.js
+++ src/components/Demo-Account.js
@@ -20,7 +20,7 @@ const DemoAccount = ({ userID, onClose }) => {
     setError(null);
 
     try {
-      const response = await fetch("http://admin.tducoin.com/api/webappuser/tradingaccount", {
+      const response = await fetch("https://admin.tducoin.com/api/webappuser/tradingaccount", {
         method: "POST",
         headers: {
           "x-api-key": "oqKbBxKcEn9l4IXE4EqS2sgNzXPFvE",
