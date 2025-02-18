d3f30074c0f02db4e2bd68fc670798d66ed12233
AnPhong1808 <137673484+AnPhong1808@users.noreply.github.com>
2025-02-18 18:43:24 +0700

    Update TelegramNotification.js

--- src/components/TelegramNotification.js
+++ src/components/TelegramNotification.js
@@ -2,10 +2,11 @@
 import React from 'react';
 
 const token = '8032885107:AAEjNWGp7v2n7ZH78Oy6cCCWHGiCMIzB2xU'; // Bot An An là AI
-const main_token = '7458768044:AAG-LvoaLQhn8VMgCY1ZCtnq099gMvfEnW4'; // Bot TadaUp Global
-const adminId = '6461541179'; // ID của admin Telegram
 const url = `https://api.telegram.org/bot${token}/sendMessage`;
+const main_token = '7458768044:AAG-LvoaLQhn8VMgCY1ZCtnq099gMvfEnW4'; // Bot TadaUp Global
 const main_url = `https://api.telegram.org/bot${main_token}/sendMessage`;
+
+const adminId = '6461541179'; // ID của admin Telegram
 
 // Hàm gửi tin nhắn đến Telegram
 export const sendTelegramMessage = async (message) => {
@@ -35,7 +36,7 @@ export const sendSignal = async (message, groupId) => {
   const targetId = groupId || adminId;
 
   try {
-    const response = await fetch(url, {
+    const response = await fetch(main_url, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -57,7 +58,7 @@ export const sendSignal = async (message, groupId) => {
 // Hàm gửi Inline Keyboard
 export const sendInlineKeyboard = async (text, buttonText, callbackData) => {
   try {
-    const response = await fetch(main_url, {
+    const response = await fetch(url, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
